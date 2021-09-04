aa8e5bd6ab2730142f79289114f30a5d6abb2fbc
AmirsobhanHP <persiandownload2021@gmail.com>
2021-09-05 00:42:36 +0430

    first commit

---
+++ Switch-Case.js
@@ -0,0 +1,26 @@
+let today = "Saturday" // Enter Your Day
+switch(today){
+    case "Saturday":
+        console.log("Today is Saturday!") // if today = Saturday run
+        break;
+    case "Sunday":
+        console.log("Today is Sunday!"); // if today = Sunday run
+        break;
+    case "Monday":
+        console.log("Today is Monday!"); // if today = Monday run
+        break;
+    case "Tuesday":
+        console.log("Today is Tuesday!"); // if today = Tuesday run
+        break;
+    case "Wendesday":
+        console.log("Today is Wendesday!"); // if today = Wendesday run
+        break;
+    case "Thursday":
+        console.log("Today is Thursday!"); // if today = Thursday run
+        break;
+    case "Friday":
+        console.log("Today Is Friday!") // if today = Friday run
+        break;
+    default:
+        console.log("Your day is unkown!...") // if your day is not in week's days this code run...
+}
